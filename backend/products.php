26ee3f223288c926848d24a72d5f095c2a887ae2
Wesley Santos <weesleyschons@gmail.com>
2022-10-25 08:52:34 -0300

    fix: remocao de linhas desnecessarias

--- backend/products.php
+++ backend/products.php
@@ -7,7 +7,5 @@ $products = $conn->query($sql);
 if(isset($_GET['delid'])) {
     $id = intval($_GET['delid']);
     $sql = $conn->query("DELETE FROM estoque where CONTROLE=$id");
-    echo "<script>alert('Dados deletados');</script>";
-    echo "<script>window.location.href = 'index.php'</script>";   
     header('Location: ../index.php');
 }
